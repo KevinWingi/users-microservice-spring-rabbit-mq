0783cde934c0f54dffbf1da7fbe8bea7ce6cd722
KevinWingi <KevinWing8@gmail.com>
2023-10-22 12:19:18 +0200

    Prepare save user method

--- src/main/java/com/kwingi/usersmicroservice/services/UserService.java
+++ src/main/java/com/kwingi/usersmicroservice/services/UserService.java
@@ -4,8 +4,10 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kwingi.usersmicroservice.entities.UserEntity;
+import com.kwingi.usersmicroservice.producers.UserProducer;
 import com.kwingi.usersmicroservice.repositories.UserRepository;
 
 @Service
@@ -13,9 +15,19 @@ public class UserService {
 	
 	@Autowired private UserRepository userRepository;
 	
+	@Autowired private UserProducer userProducer;
+		
+	@Transactional
 	public UserEntity save(UserEntity user) {
+		if (userRepository.existsByEmail(user.getEmail()))
+			throw new IllegalArgumentException(String.format("Already exist user with %s", user.getEmail()));
+		
 		user.setId(UUID.randomUUID().toString());
-		return userRepository.save(user);
+		UserEntity createdUser = userRepository.save(user);
+		
+		userProducer.sendWelcomeEmail(createdUser);
+		
+		return createdUser;
 	}
 	
 }
